284662b35b52ea9b57063ac64ffabc385510dd49
Huynh Duc Dung <dunghd.it@gmail.com>
2023-10-04 21:00:39 +0800

    chore: format code with biome

--- app/index.ts
+++ app/index.ts
@@ -15,7 +15,7 @@ const isVercel = process.env.DEPLOYMENT_ENV === "vercel";
 // middleware routes
 const isProduction = process.env.NODE_ENV === "production";
 if (isProduction) {
-  app.use(helmet());
+	app.use(helmet());
 }
 app.use(cors());
 app.use(bodyParser.json());
@@ -23,34 +23,34 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(jsend.middleware); // more detail on https://github.com/omniti-labs/jsend
 app.use(errorHandler);
 const options = {
-  dotfiles: "ignore",
-  etag: false,
-  extensions: ["htm", "html"],
-  index: false,
-  maxAge: "1d",
-  redirect: false,
-  setHeaders(res, _path, _stat) {
-    res.set("x-static-timestamp", Date.now());
-  },
+	dotfiles: "ignore",
+	etag: false,
+	extensions: ["htm", "html"],
+	index: false,
+	maxAge: "1d",
+	redirect: false,
+	setHeaders(res, _path, _stat) {
+		res.set("x-static-timestamp", Date.now());
+	},
 };
 
 app.use("/static", express.static(join(__dirname, "../public"), options));
 
 app.get("/", (_req, res) => {
-  res.send("Open Swagger UI at http://localhost:3000/static/index.html");
+	res.send("Open Swagger UI at http://localhost:3000/static/index.html");
 });
 
 app.get("/api", (_req, res) => {
-  res.setHeader("Content-Type", "application/json");
-  res.json({ name: "Hello world" });
+	res.setHeader("Content-Type", "application/json");
+	res.json({ name: "Hello world" });
 });
 
 app.use("/api/todo", todoRoutes);
 
 if (!isVercel) {
-  app.listen(3000).on("listening", () => {
-    logger.info("server is listening on port http://localhost:3000");
-  });
+	app.listen(3000).on("listening", () => {
+		logger.info("server is listening on port http://localhost:3000");
+	});
 }
 
 module.exports = app;
--- app/routes/todo.ts
+++ app/routes/todo.ts
@@ -6,76 +6,76 @@ const router = Router();
 let todos: Todo[] = [];
 
 router.post("/", (req, res, _next) => {
-  const text = (req.body as { text: string }).text;
-  const newTodo = new Todo(Math.random().toString(), text);
-  todos.push(newTodo);
+	const text = (req.body as { text: string }).text;
+	const newTodo = new Todo(Math.random().toString(), text);
+	todos.push(newTodo);
 
-  res
-    .status(201)
-    .jsend.success({ message: "Created the todo.", createdTodo: newTodo });
+	res
+		.status(201)
+		.jsend.success({ message: "Created the todo.", createdTodo: newTodo });
 });
 
 router.get("/", (_req, res, _next) => {
-  res.jsend.success({ todos: todos });
+	res.jsend.success({ todos: todos });
 });
 
 router.patch("/:id", (req, res, _next) => {
-  const todoId = req.params.id;
-  const updatedText = (req.body as { text: string }).text;
+	const todoId = req.params.id;
+	const updatedText = (req.body as { text: string }).text;
 
-  const todoIndex = todos.findIndex((todo) => todo.id === todoId);
+	const todoIndex = todos.findIndex((todo) => todo.id === todoId);
 
-  if (todoIndex < 0) {
-    res.jsend.error({ message: "Could not find todo!", code: 500 });
-    return;
-  }
+	if (todoIndex < 0) {
+		res.jsend.error({ message: "Could not find todo!", code: 500 });
+		return;
+	}
 
-  todos[todoIndex] = new Todo(
-    todos[todoIndex].id,
-    updatedText,
-    todos[todoIndex].completed
-  );
-  res.jsend.success({ message: "Updated!", updatedTodo: todos[todoIndex] });
+	todos[todoIndex] = new Todo(
+		todos[todoIndex].id,
+		updatedText,
+		todos[todoIndex].completed,
+	);
+	res.jsend.success({ message: "Updated!", updatedTodo: todos[todoIndex] });
 });
 
 router.put("/:id", (req, res, _next) => {
-  const todoId = req.params.id;
-  const updatedText = (req.body as { text: string }).text;
-  const todoIndex = todos.findIndex((todo) => todo.id === todoId);
+	const todoId = req.params.id;
+	const updatedText = (req.body as { text: string }).text;
+	const todoIndex = todos.findIndex((todo) => todo.id === todoId);
 
-  if (todoIndex < 0) {
-    res.jsend.error({ message: "Could not find todo!", code: 500 });
-    return;
-  }
+	if (todoIndex < 0) {
+		res.jsend.error({ message: "Could not find todo!", code: 500 });
+		return;
+	}
 
-  todos[todoIndex] = new Todo(
-    todos[todoIndex].id,
-    updatedText,
-    todos[todoIndex].completed
-  );
-  res.jsend.success({ message: "Updated!", updatedTodo: todos[todoIndex] });
+	todos[todoIndex] = new Todo(
+		todos[todoIndex].id,
+		updatedText,
+		todos[todoIndex].completed,
+	);
+	res.jsend.success({ message: "Updated!", updatedTodo: todos[todoIndex] });
 });
 
 router.delete("/:id", (req, res, _next) => {
-  const todoId = req.params.id;
-  todos = todos.filter((todo) => todo.id !== todoId);
-  res.jsend.success({ message: "Todo deleted!" });
+	const todoId = req.params.id;
+	todos = todos.filter((todo) => todo.id !== todoId);
+	res.jsend.success({ message: "Todo deleted!" });
 });
 
 router.post("/:id/complete", (req, res, _next) => {
-  const todoId = req.params.id;
-  const todoIndex = todos.findIndex((todo) => todo.id === todoId);
+	const todoId = req.params.id;
+	const todoIndex = todos.findIndex((todo) => todo.id === todoId);
 
-  if (todoIndex < 0) {
-    res.jsend.error({ message: "Could not find todo!", code: 500 });
-    return;
-  }
+	if (todoIndex < 0) {
+		res.jsend.error({ message: "Could not find todo!", code: 500 });
+		return;
+	}
 
-  todos[todoIndex] = new Todo(todos[todoIndex].id, todos[todoIndex].text, true);
-  res.jsend.success({
-    message: "Completed the todo!",
-    completedTodo: todos[todoIndex],
-  });
+	todos[todoIndex] = new Todo(todos[todoIndex].id, todos[todoIndex].text, true);
+	res.jsend.success({
+		message: "Completed the todo!",
+		completedTodo: todos[todoIndex],
+	});
 });
 
 export default router;
