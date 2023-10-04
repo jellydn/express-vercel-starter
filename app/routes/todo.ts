17a58ff49a043a365e1b6899b5f4d2360de498c4
Huynh Duc Dung <dunghd.it@gmail.com>
2023-10-04 21:45:32 +0800

    feat: add typing for todo route

--- app/routes/todo.ts
+++ app/routes/todo.ts
@@ -42,7 +42,7 @@ router.get("/", (_req, res, _next) => {
 	res.jsend.success({ todos: todos });
 });
 
-router.patch("/:id", (req, res, _next) => {
+router.patch("/:id", (req: Request<TodoParams>, res: Response, _next) => {
 	const todoId = req.params.id;
 	const updatedText = (req.body as { text: string }).text;
 
@@ -61,7 +61,7 @@ router.patch("/:id", (req, res, _next) => {
 	res.jsend.success({ message: "Updated!", updatedTodo: todos[todoIndex] });
 });
 
-router.put("/:id", (req, res, _next) => {
+router.put("/:id", (req: Request<TodoParams>, res: Response, _next) => {
 	const todoId = req.params.id;
 	const updatedText = (req.body as { text: string }).text;
 	const todoIndex = todos.findIndex((todo) => todo.id === todoId);
@@ -79,26 +79,33 @@ router.put("/:id", (req, res, _next) => {
 	res.jsend.success({ message: "Updated!", updatedTodo: todos[todoIndex] });
 });
 
-router.delete("/:id", (req, res, _next) => {
+router.delete("/:id", (req: Request<TodoParams>, res: Response, _next) => {
 	const todoId = req.params.id;
 	todos = todos.filter((todo) => todo.id !== todoId);
 	res.jsend.success({ message: "Todo deleted!" });
 });
 
-router.post("/:id/complete", (req, res, _next) => {
-	const todoId = req.params.id;
-	const todoIndex = todos.findIndex((todo) => todo.id === todoId);
+router.post(
+	"/:id/complete",
+	(req: Request<TodoParams>, res: Response, _next) => {
+		const todoId = req.params.id;
+		const todoIndex = todos.findIndex((todo) => todo.id === todoId);
 
-	if (todoIndex < 0) {
-		res.jsend.error({ message: "Could not find todo!", code: 500 });
-		return;
-	}
+		if (todoIndex < 0) {
+			res.jsend.error({ message: "Could not find todo!", code: 500 });
+			return;
+		}
 
-	todos[todoIndex] = new Todo(todos[todoIndex].id, todos[todoIndex].text, true);
-	res.jsend.success({
-		message: "Completed the todo!",
-		completedTodo: todos[todoIndex],
-	});
-});
+		todos[todoIndex] = new Todo(
+			todos[todoIndex].id,
+			todos[todoIndex].text,
+			true,
+		);
+		res.jsend.success({
+			message: "Completed the todo!",
+			completedTodo: todos[todoIndex],
+		});
+	},
+);
 
 export default router;
