e53bdb3cc7a20c78983f1b3b223efe0c3b4786c9
Dung Huynh Duc <dunghd.it@gmail.com>
2024-10-01 07:23:53 +0800

    feat: remove Vercel-specific deployment check

--- app/index.ts
+++ app/index.ts
@@ -1,11 +1,8 @@
 import logger from "../logger";
 import { app } from "./app";
 
-const isVercel = process.env.DEPLOYMENT_ENV === "vercel";
-if (!isVercel) {
-	app.listen(3000).on("listening", () => {
-		logger.info("server is listening on port http://localhost:3000");
-	});
-}
+app.listen(3000).on("listening", () => {
+	logger.info("server is listening on port http://localhost:3000");
+});
 
 module.exports = app;
